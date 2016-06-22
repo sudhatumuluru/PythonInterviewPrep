3490042053dc4c6e6c69afec1ce9f318ce95e0f1
sudhatumuluru <sreesudha.tumuluru@gmail.com>
2016-06-21 21:42:56 -0700

    Time value is returned in seconds

--- Time.py
+++ Time.py
@@ -6,4 +6,4 @@ print(time.time())
 
 # Here I’m calling time.time() on February 27, 2015, at 11:05 Pacific Standard Time, or 7:05 PM UTC.
 # The return value is how many seconds have passed between the Unix epoch (12 AM on January 1, 1970, Coordinated Universal Time (UTC))
-# and the moment time.time() was called.s
+# and the moment time.time() was called.
