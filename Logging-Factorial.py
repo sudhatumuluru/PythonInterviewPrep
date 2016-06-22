e1bdbb7e85916bb3f26c61b922216bb7e2c4f175
sudhatumuluru <sreesudha.tumuluru@gmail.com>
2016-06-21 20:56:46 -0700

    Explaining Logging with factorial program

--- Logging-Factorial.py
+++ Logging-Factorial.py
@@ -1,5 +1,6 @@
 import logging
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+
 logging.debug('Start of program')
 
 def factorial(n):
@@ -12,4 +13,21 @@ def factorial(n):
     return total
 
 print(factorial(5))
+
+logging.debug('End of program')
+
+# Correction in the above code
+logging.debug('Start of program')
+
+def factorial(n):
+    logging.debug('Start of factorial(%s)' % (n))
+    total = 1
+    for i in range(1,n + 1):
+        total *= i
+        logging.debug('i is ' + str(i) + ', total is ' + str(total))
+    logging.debug('End of factorial(%s)' % (n))
+    return total
+
+print(factorial(5))
+
 logging.debug('End of program')
